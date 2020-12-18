ea625bb2286f07a6de6a2fe5f3d12c93d06f21a8
CrimsonDragon47 <41874831+CrimsonDragon47@users.noreply.github.com>
2020-12-18 14:54:25 +0000

    format

--- java/Looper.java
+++ java/Looper.java
@@ -16,182 +16,173 @@ public class Looper implements Runnable {
         Looper.start(out);
         out.println("Terminated.");
     }
-    
-    public static void start(PrintStream out){
-      
-      Looper[] looper = new Looper[totalThreads];
-      for(int i=0;i<totalThreads;i++) looper[i] = new Looper(out,i);
-      boolean complete = false;
-      do{
-        try{
-          for(Looper l:looper) l.getThread().join();
-          complete=true;
-        }catch(InterruptedException e){
-          out.print('.');
+
+    public static void start(PrintStream out) {
+
+        Looper[] looper = new Looper[totalThreads];
+        for (int i = 0; i < totalThreads; i++) looper[i] = new Looper(out, i);
+        boolean complete = false;
+        do {
+            try {
+                for (Looper l : looper) l.getThread().join();
+                complete = true;
+            } catch (InterruptedException e) {
+                out.print('.');
+            }
+        } while (!complete);
+
+        List<Element> loops = new ArrayList();
+        long totalCount = 0l;
+        for (int i = 0; i < totalThreads; i++) {
+            loops.addAll(looper[i].getLoops());
+            totalCount += looper[i].getTotalCount();
+            looper[i] = null;
         }
-      }while(!complete);
-      
-      List<Element> loops = new ArrayList();
-      long totalCount = 0l;
-      for(int i=0;i<totalThreads;i++){
-        loops.addAll(looper[i].getLoops());
-        totalCount += looper[i].getTotalCount();
-        looper[i] = null;
-      }
-      out.printf("%nS:%d%nC:%d%n=%.2f%n%n", loops.size(), totalCount, totalCount/(float)CHECKSUM);
-      
-      
-      //TODO
+        out.printf("%nS:%d%nC:%d%n=%.2f%n%n", loops.size(), totalCount, totalCount / (float) CHECKSUM);
+
+
+        //TODO
     }
 
 
-
-
-
-
     /////////
-    
-    
 
-    
 
     private PrintStream out;
     private List<Element> loops = new ArrayList();
     private long totalCount = 0;
-    
+
     private int threadId;
     private Thread t;
 
     private Looper(PrintStream out, int threadId) {
         this.out = out;
-        this.threadId =threadId;
+        this.threadId = threadId;
         this.t = new Thread(this, String.format("[Looper-%d]", threadId));
         t.start();
     }
-    
+
     //todo LoopMaster to analyse data
-    
-    public Thread getThread(){
-       return t;
+
+    public Thread getThread() {
+        return t;
     }
-    
-    public List<Element> getLoops(){
-      return loops;
+
+    public List<Element> getLoops() {
+        return loops;
     }
-    
-    public long getTotalCount(){
-      return totalCount;
+
+    public long getTotalCount() {
+        return totalCount;
     }
 
     @Override
     public void run() {
-      out.printf("%s started.%n",t.getName());
-      for(int len=2+threadId;len<13;len+=totalThreads){
-        int[] loop = new int[len];
-        for(int i=0;i<len;i++) loop[i]=0;
-        
-        boolean complete=false; 
-        do{
-          try{
-            
-            loop = nextLoop(loop,len,0);
-            Element e = new Element(loop, len);
-            loops.add(e);
-            totalCount+=e.count;
-          }catch(IllegalArgumentException e){
-            complete=true;
-          }
-        }while(!complete);
-          
-        out.println(totalCount);
-      }
-      
-      out.printf("%s done.%n", t.getName());
-      
-      
-        
-    }
-    
-    
-    
-    private int[] nextLoop(int[] loop, int len, int i){
-    
-      if(i==len-1){
-        int[] loop2 = new int[len];
-         
-        if(loop[i]>0){
-          loop2[i]=loop[i]-1;
-        }else{
-          loop2[i]=10;
+        out.printf("%s started.%n", t.getName());
+        for (int len = 2 + threadId; len < 13; len += totalThreads) {
+            int[] loop = new int[len];
+            for (int i = 0; i < len; i++) loop[i] = 0;
+
+            boolean complete = false;
+            do {
+                try {
+
+                    loop = nextLoop(loop, len, 0);
+                    Element e = new Element(loop, len);
+                    loops.add(e);
+                    totalCount += e.count;
+                } catch (IllegalArgumentException e) {
+                    complete = true;
+                }
+            } while (!complete);
+
+            out.println(totalCount);
         }
-        
-        return loop2;
-        
-      }else if(i>0){
-        
-          
-          int[] loop2;
-          if(loop[i]>0){//first 
-            loop2 = nextLoop(loop,len,i+1);
-            if(loop2[i+1]>0){
-              loop2[i]=loop[i];
-            }else{
-              loop2 = nextLoop(loop2,len,i+1);
-              loop2[i]=loop[i]-1;
+
+        out.printf("%s done.%n", t.getName());
+
+
+    }
+
+
+    private int[] nextLoop(int[] loop, int len, int i) {
+
+        if (i == len - 1) {
+            int[] loop2 = new int[len];
+
+            if (loop[i] > 0) {
+                loop2[i] = loop[i] - 1;
+            } else {
+                loop2[i] = 10;
             }
-          }else{//second 
-            loop2=loop;
-            if(loop2[i+1]==0){
-              loop2 = nextLoop(loop,len,i+1);
-            } 
-            loop2[i]=10;
-          }
-          return loop2;
-        
-      }else{//i==0
-      
-        //for(int value:loop) out.print(value);
-        //out.println();
-        
-        boolean complete=false;
-        int[] loop2 = loop;
-        do{
-          loop2 = nextLoop(loop2,len,i+1);
-          loop2[0]=0;
-          if(loop2[i+1]>0){
-            int sum=0;
-            for(int k=1;k<len;k++) sum+=loop2[k];
-            loop2[0] = ((sum/13)+1)*13-sum;
-            
-          }else{
-            throw new IllegalArgumentException("next len");
-          }
-          
-          if(loop2[0]<11&&isPossible(loop2,len)){
-            complete=true;
-          }
-        }while(!complete);
-        return loop2;
-      }
-    }
-    
-    private boolean isPossible(int[] loop, int len){
-      //out.print('.');
-      boolean[] hit = new boolean[13];
-      Arrays.fill(hit, false);
-      int[] remaining = Arrays.copyOf(VALUE_COUNT, 13);
-      int index=0;
-      for(int value:loop){
-        if(--remaining[value]<0) return false;
-        index=(index+value)%13;
-        if(hit[index]){
-          return false;
-        }else{
-          hit[index]=true;
+
+            return loop2;
+
+        } else if (i > 0) {
+
+
+            int[] loop2;
+            if (loop[i] > 0) {//first
+                loop2 = nextLoop(loop, len, i + 1);
+                if (loop2[i + 1] > 0) {
+                    loop2[i] = loop[i];
+                } else {
+                    loop2 = nextLoop(loop2, len, i + 1);
+                    loop2[i] = loop[i] - 1;
+                }
+            } else {//second
+                loop2 = loop;
+                if (loop2[i + 1] == 0) {
+                    loop2 = nextLoop(loop, len, i + 1);
+                }
+                loop2[i] = 10;
+            }
+            return loop2;
+
+        } else {//i==0
+
+            //for(int value:loop) out.print(value);
+            //out.println();
+
+            boolean complete = false;
+            int[] loop2 = loop;
+            do {
+                loop2 = nextLoop(loop2, len, i + 1);
+                loop2[0] = 0;
+                if (loop2[i + 1] > 0) {
+                    int sum = 0;
+                    for (int k = 1; k < len; k++) sum += loop2[k];
+                    loop2[0] = ((sum / 13) + 1) * 13 - sum;
+
+                } else {
+                    throw new IllegalArgumentException("next len");
+                }
+
+                if (loop2[0] < 11 && isPossible(loop2, len)) {
+                    complete = true;
+                }
+            } while (!complete);
+            return loop2;
         }
-      }
-      if(index>0) return false;
-      return true;
+    }
+
+    private boolean isPossible(int[] loop, int len) {
+        //out.print('.');
+        boolean[] hit = new boolean[13];
+        Arrays.fill(hit, false);
+        int[] remaining = Arrays.copyOf(VALUE_COUNT, 13);
+        int index = 0;
+        for (int value : loop) {
+            if (--remaining[value] < 0) return false;
+            index = (index + value) % 13;
+            if (hit[index]) {
+                return false;
+            } else {
+                hit[index] = true;
+            }
+        }
+        if (index > 0) return false;
+        return true;
     }
 
     private static long factorial(int x) {
@@ -212,34 +203,36 @@ public class Looper implements Runnable {
             this.len = len;
             this.count = getCount();
         }
-        
-        private long getCount(){
-      long count = 1l;
-      int[] remaining = Arrays.copyOf(VALUE_COUNT, 13);
-      for(int i=0;i<len;i++) remaining[loop[i]]--;
-      
-      count *= factorial(4) / factorial(remaining[10]);
-      
-      
-      int[] value = new int[13-len];
-      int j=0;
-      for(int k=1;k<14;k++){
-        int v=k;
-        if(v>10) v=10;
-        if(--remaining[v]>=0){
-          value[j] = v;
-          j++;
+
+        private long getCount() {
+            long count = 1l;
+            int[] remaining = Arrays.copyOf(VALUE_COUNT, 13);
+            for (int i = 0; i < len; i++) remaining[loop[i]]--;
+
+            count *= factorial(4) / factorial(remaining[10]);
+
+
+            int[] value = new int[13 - len];
+            {
+                int j = 0;
+                for (int k = 1; k < 14; k++) {
+                    int v = k;
+                    if (v > 10) v = 10;
+                    if (--remaining[v] >= 0) {
+                        value[j] = v;
+                        j++;
+                    }
+                }
+            }
+
+            int x = 1;
+
+            //TODO
+
+            count *= x;
+
+            return count;
         }
-      }
-      
-      int x=1;
-      
-      //TODO
-      
-      count *= x;
-      
-      return count;
-    }
 
     }
 }
